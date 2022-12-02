0dc3107961c237839d8bc690cfaae7b2202513eb
Peter <petr.ovchinnicov@gmail.com>
2022-12-02 18:08:36 +0300

    Pre-final commit

--- calorie counter.py
+++ calorie counter.py
@@ -6,151 +6,182 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox
 
 
-class CalorieCounter(QMainWindow):
+class CalorieCounter(QMainWindow):  # главное окно
     def __init__(self):
         super().__init__()
-        uic.loadUi('./source/calorie counter.ui', self)
+        uic.loadUi('./source/calorie counter.ui', self)  # загрузка интерфейса
 
-        self.con = sqlite3.connect('./source/calorie counter db.db')
+        self.con = sqlite3.connect('./source/calorie counter db.db')  # подключение БД
         cur = self.con.cursor()
 
-        row = cur.execute('SELECT * FROM user').fetchone()
-        self.calorie_norm, self.protein_norm, self.fat_norm, self.carbohydrate_norm = row
-        self.total_calorie, self.total_protein, self.total_fat, self.total_carbohydrate = 0, 0, 0, 0
+        row = cur.execute('SELECT * FROM user').fetchone()  # данные пользователя
+        self.calorie_norm, self.protein_norm, self.fat_norm, self.carbohydrate_norm = row  # норма в день
+        self.total_calorie, self.total_protein, self.total_fat, self.total_carbohydrate = 0, 0, 0, 0  # общие КБЖУ
 
-        dates = cur.execute('SELECT date FROM dates').fetchall()
-        for i in dates:
-            self.text.appendPlainText(i[0])
-            food = cur.execute('SELECT name FROM food WHERE date_id = (SELECT id FROM dates WHERE id = date_id)')
-            for j in food:
-                self.text.appendPlainText(j[0])
+        dates = cur.execute('SELECT date FROM dates').fetchall()  # все даты
+        if len(dates):  # проверка на наличие дат
+            with open('./source/list of food.csv', encoding='utf8') as file:
+                food_list = list(csv.reader(file, delimiter=','))[1:]  # csv таблица с продуктами (без первого ряда)
+                for i in dates:
+                    day_calorie, day_protein, day_fat, day_carbohydrate = 0, 0, 0, 0  # КБЖУ в день
+                    food_text = list()  # список строчек
+                    food = cur.execute('SELECT name, amount FROM food WHERE date_id = '
+                                       f'(SELECT id FROM dates WHERE date = "{i[0]}")').fetchall()  # продукты за дату
+                    for j in range(len(food)):
+                        # КБЖУ
+                        characteristics = list(filter(lambda x: x[0] == food[j][0], food_list))[0]
+                        calorie = round(float(characteristics[4]) / 100 * food[j][1])
+                        protein = round(float(characteristics[1]) / 100 * food[j][1])
+                        fat = round(float(characteristics[2]) / 100 * food[j][1])
+                        carbohydrate = round(float(characteristics[3]) / 100 * food[j][1])
 
-        with open('./source/list of food.csv', encoding='utf8') as file:
-            reader = csv.reader(file, delimiter=',')
-            food = cur.execute('SELECT name, amount FROM food')
-            for i in food:
-                for j in list(reader)[1:]:
-                    if i[0] == j[0]:
-                        self.total_calorie += round(float(j[4]) / 100 * i[1])
-                        self.total_protein += round(float(j[1]) / 100 * i[1])
-                        self.total_fat += round(float(j[2]) / 100 * i[1])
-                        self.total_carbohydrate += round(float(j[3]) / 100 * i[1])
+                        # добавление в обшее значение
+                        self.total_calorie += calorie
+                        self.total_protein += protein
+                        self.total_fat += fat
+                        self.total_carbohydrate += carbohydrate
 
-        dates = len(dates)
-        if not dates:
-            dates = 1
-        else:
+                        # добавление в дневное значение
+                        day_calorie += calorie
+                        day_protein += protein
+                        day_fat += protein
+                        day_carbohydrate += carbohydrate
+
+                        # добавление в список строчек
+                        food_text.append(f'{food[j][0]} - {food[j][1]} г, Калории - {calorie}, Белки'
+                                         f' - {protein}, Жиры - {fat}, Углеводы - {carbohydrate}')
+                    self.text.appendPlainText(f'{i[0]}: Калории - {day_calorie}, Белки - {day_protein}, '
+                                              f'Жиры - {day_fat}, Углеводы - {day_carbohydrate}')  # вывод даты
+                    for j in food_text:  # вывод продуктов
+                        self.text.appendPlainText(j)
+            dates = len(dates)
+            # включение кнопок
             self.add_day_btn.setEnabled(True)
             self.add_product_btn.setEnabled(True)
+        else:
+            dates = 1  # защита от деления на 0
+
+        # изменение статистики
         self.statistics.setText(
             f'Калории (ккал): Рекомендуемые в день - {self.calorie_norm}, '
-            f'Средние в день - {round(self.total_calorie / dates)};\n'
+            f'Средние в день - {round(self.total_calorie / dates)}\n'
             f'Белки (г): Рекомендуемые в день - {self.protein_norm}, '
-            f'Средние в день - {round(self.total_protein / dates)};\n'
+            f'Средние в день - {round(self.total_protein / dates)}\n'
             f'Жиры (г): Рекомендуемые в день - {self.fat_norm}, '
-            f'Средние в день - {round(self.total_fat / dates)};\n'
+            f'Средние в день - {round(self.total_fat / dates)}\n'
             f'Углеводы (г): Рекомендуемые в день - {self.carbohydrate_norm}, '
             f'Средние в день - {round(self.total_carbohydrate / dates)}')
 
+        # триггеры
         self.add_day_btn.clicked.connect(self.add_day)
         self.add_product_btn.clicked.connect(self.add_product)
         self.edit_characteristics_btn.clicked.connect(self.edit_characteristics)
 
-    def add_day(self):
+    def add_day(self):  # добавление дня
+        # вывод диалога
         dialog = DayAddDialog()
         dialog.exec()
 
         if dialog.clicked:
-            self.text.setPlainText('')
-
             cur = self.con.cursor()
             dates = cur.execute('SELECT date FROM dates').fetchall()
-            if dialog.date not in map(lambda x: x[0], dates):
-                cur.execute(f'INSERT INTO dates(date) VALUES("{dialog.date}")')
-                self.con.commit()
 
-            dates = cur.execute('SELECT date FROM dates').fetchall()
-            for i in dates:
-                self.text.appendPlainText(i[0])
-                food = cur.execute(
-                    f'SELECT name FROM food WHERE date_id = (SELECT id FROM dates WHERE date = "{i[0]}")').fetchall()
-                for j in food:
-                    self.text.appendPlainText(j[0])
+            if dialog.date not in map(lambda x: x[0], dates):  # проверка на повтор даты
+                cur.execute(f'INSERT INTO dates(date) VALUES("{dialog.date}")')  # добавление
+                self.con.commit()
+                # вывод даты
+                self.text.appendPlainText(f'{dialog.date}: Калории - 0, Белки - 0, Жиры - 0, Углеводы - 0')
 
             cur.close()
 
-        self.add_product_btn.setEnabled(True)
+        self.add_product_btn.setEnabled(True)  # включение кнопки
 
-    def add_product(self):
+    def add_product(self):  # добавление продукта
         dialog = ProductAddDialog()
         dialog.exec()
 
         if dialog.clicked:
-            self.text.setPlainText('')
-
-            with open('./source/list of food.csv', encoding='utf8') as file:
-                reader = csv.reader(file, delimiter=',')
-                for i in list(reader)[1:]:
-                    if i[0] == dialog.product:
-                        product = i
-                        break
-                self.total_calorie += round(float(product[4]) / 100 * dialog.amount)
-                self.total_protein += round(float(product[1]) / 100 * dialog.amount)
-                self.total_fat += round(float(product[2]) / 100 * dialog.amount)
-                self.total_carbohydrate += round(float(product[3]) / 100 * dialog.amount)
+            self.text.setPlainText('')  # очистка предыдущего вывода
 
             cur = self.con.cursor()
             cur.execute(
                 f'INSERT INTO food(date_id, name, amount) VALUES((SELECT id FROM dates WHERE date = "{dialog.day}"), '
-                f'"{product[0]}", {dialog.amount})')
+                f'"{dialog.product}", {dialog.amount})')  # добавление продукта в ДБ
             self.con.commit()
 
+            self.total_calorie, self.total_protein, self.total_fat, self.total_carbohydrate = 0, 0, 0, 0
             dates = cur.execute('SELECT date FROM dates').fetchall()
-            for i in dates:
-                self.text.appendPlainText(i[0])
-                food = cur.execute(
-                    f'SELECT name FROM food WHERE date_id = (SELECT id FROM dates WHERE date = "{i[0]}")').fetchall()
-                for j in food:
-                    self.text.appendPlainText(j[0])
+            with open('./source/list of food.csv', encoding='utf8') as file:
+                reader = list(csv.reader(file, delimiter=','))[1:]
+                for i in dates:
+                    day_calorie, day_protein, day_fat, day_carbohydrate = 0, 0, 0, 0
+                    food_text = list()
+                    food = cur.execute(
+                        'SELECT name, amount FROM food WHERE date_id = '
+                        f'(SELECT id FROM dates WHERE date = "{i[0]}")').fetchall()
+                    for j in range(len(food)):
+                        characteristics = list(filter(lambda x: x[0] == reader[j][0], reader))[0]
+                        calorie = round(float(characteristics[4]) / 100 * food[j][1])
+                        protein = round(float(characteristics[1]) / 100 * food[j][1])
+                        fat = round(float(characteristics[2]) / 100 * food[j][1])
+                        carbohydrate = round(float(characteristics[3]) / 100 * food[j][1])
+
+                        self.total_calorie += calorie
+                        self.total_protein += protein
+                        self.total_fat += fat
+                        self.total_carbohydrate += carbohydrate
+
+                        day_calorie += calorie
+                        day_protein += protein
+                        day_fat += protein
+                        day_carbohydrate += carbohydrate
+
+                        food_text.append(f'{food[j][0]} - {food[j][1]} г, Калории - {calorie}, Белки'
+                                         f' - {protein}, Жиры - {fat}, Углеводы - {carbohydrate}')
+                    self.text.appendPlainText(f'{i[0]}: Калории - {day_calorie}, Белки - {day_protein}, '
+                                              f'Жиры - {day_fat}, Углеводы - {day_carbohydrate}')
+                    for j in food_text:
+                        self.text.appendPlainText(j)
 
             dates = len(dates)
             self.statistics.setText(
                 f'Калории (ккал): Рекомендуемые в день - {self.calorie_norm}, '
-                f'Средние в день - {round(self.total_calorie / dates)};\n'
+                f'Средние в день - {round(self.total_calorie / dates)}\n'
                 f'Белки (г): Рекомендуемые в день - {self.protein_norm}, '
-                f'Средние в день - {round(self.total_protein / dates)};\n'
+                f'Средние в день - {round(self.total_protein / dates)}\n'
                 f'Жиры (г): Рекомендуемые в день - {self.fat_norm}, '
-                f'Средние в день - {round(self.total_fat / dates)};\n'
+                f'Средние в день - {round(self.total_fat / dates)}\n'
                 f'Углеводы (г): Рекомендуемые в день - {self.carbohydrate_norm}, '
                 f'Средние в день - {round(self.total_carbohydrate / dates)}')
 
             cur.close()
 
-    def edit_characteristics(self):
+    def edit_characteristics(self):  # изменение характеристики
         dialog = EditCharacteristicsDialog()
         dialog.exec()
 
         if dialog.clicked:
+            # изменение нормы
             self.calorie_norm = round(dialog.calorie)
             self.protein_norm = round(dialog.protein)
             self.fat_norm = round(dialog.fat)
             self.carbohydrate_norm = round(dialog.carbohydrate)
             self.con.cursor().execute(
                 f'UPDATE user SET calorie_norm = {self.carbohydrate_norm}, protein_norm = {self.protein_norm}, '
-                f'fat_norm = {self.fat_norm}, carbohydrate_norm = {self.carbohydrate_norm}')
+                f'fat_norm = {self.fat_norm}, carbohydrate_norm = {self.carbohydrate_norm}')  # добавление в ДБ
 
-        days = len(self.con.cursor().execute('SELECT date FROM dates').fetchall())
-        if not days:
-            days = 1
+        dates = len(self.con.cursor().execute('SELECT date FROM dates').fetchall())
+        if not dates:
+            dates = 1
         self.statistics.setText(
-            f'Калории (ккал): Общие - {self.total_calorie}, Рекомендуемые в день - {self.calorie_norm}, '
-            f'Средние в день - {round(self.total_calorie / days)}\n'
-            f'Белки (г): Общие - {self.total_protein}, Рекомендуемые в день - {self.protein_norm}, '
-            f'Средние в день - {round(self.total_protein / days)}\n'
-            f'Жиры (г): Общие - {self.total_fat}, Рекомендуемые в день - {self.fat_norm}, '
-            f'Средние в день - {round(self.total_fat / days)}\n'
-            f'Углеводы (г): Общие - {self.total_carbohydrate}, Рекомендуемые в день - {self.carbohydrate_norm}, '
-            f'Средние в день - {round(self.total_carbohydrate / days)}')
+            f'Калории (ккал): Рекомендуемые в день - {self.calorie_norm}, '
+            f'Средние в день - {round(self.total_calorie / dates)}\n'
+            f'Белки (г): Рекомендуемые в день - {self.protein_norm}, '
+            f'Средние в день - {round(self.total_protein / dates)}\n'
+            f'Жиры (г): Рекомендуемые в день - {self.fat_norm}, '
+            f'Средние в день - {round(self.total_fat / dates)}\n'
+            f'Углеводы (г): Рекомендуемые в день - {self.carbohydrate_norm}, '
+            f'Средние в день - {round(self.total_carbohydrate / dates)}')
 
         self.add_day_btn.setEnabled(True)
 
@@ -158,17 +189,17 @@ class CalorieCounter(QMainWindow):
         dialog = ExitDialog()
         dialog.exec()
 
-        if dialog.no_clicked:
+        if dialog.no_clicked:  # удаление из БД если нажали кнопку нет
             cur = self.con.cursor()
             cur.execute('DELETE FROM dates')
             cur.execute('DELETE FROM food')
             cur.execute('UPDATE user SET calorie_norm = 0, protein_norm = 0, fat_norm = 0, carbohydrate_norm = 0')
             self.con.commit()
             self.con.close()
-        event.accept()
+        event.accept()  # закрытие
 
 
-class DayAddDialog(QDialog):
+class DayAddDialog(QDialog):  # диалог добавления дня
     def __init__(self):
         super().__init__()
         uic.loadUi('./source/day add dialog.ui', self)
@@ -176,12 +207,12 @@ class DayAddDialog(QDialog):
         self.add_btn.clicked.connect(self.click)
 
     def click(self):
-        self.clicked = True
-        self.date = self.calendar.selectedDate().toString('dd/MM/yyyy')
-        self.close()
+        self.clicked = True  # кнопка "добавить" нажата
+        self.date = self.calendar.selectedDate().toString('dd/MM/yyyy')  # дата
+        self.close() # закрытие диалога
 
 
-class ProductAddDialog(QDialog):
+class ProductAddDialog(QDialog):  # диалог добавления продукта
     def __init__(self):
         super().__init__()
         uic.loadUi('./source/product add dialog.ui', self)
@@ -203,28 +234,29 @@ class ProductAddDialog(QDialog):
     def click(self):
         self.clicked = True
 
+        # информация о приёме пищи
         self.product = self.product_combo_box.currentText()
         self.amount = self.amount_spin_box.value()
         self.day = self.day_combo_box.currentText()
 
         self.close()
 
-    def search_food(self):
+    def search_food(self):  # поиск продукта
         with open('./source/list of food.csv', encoding='utf8') as file:
             reader = map(lambda x: x[0], list(csv.reader(file, delimiter=','))[1:])
             tag = self.product_line_edit.text().lower()
 
-            self.product_combo_box.clear()
-            self.product_combo_box.addItems(list(filter(lambda x: tag in x.lower(), reader)))
+            self.product_combo_box.clear()  # очистка комбобокса
+            self.product_combo_box.addItems(list(filter(lambda x: tag in x.lower(), reader)))  # добавление в комбобокс
 
-    def search_day(self):
+    def search_day(self):  # поиск дня
         self.day_combo_box.clear()
         dates = map(lambda x: x[0], self.con.cursor().execute(
             f'SELECT date FROM dates WHERE date LIKE "%{self.day_line_edit.text()}%"').fetchall())
         self.day_combo_box.addItems(dates)
 
 
-class EditCharacteristicsDialog(QDialog):
+class EditCharacteristicsDialog(QDialog):  # диалог изменения характеристики
     def __init__(self):
         super().__init__()
         uic.loadUi('./source/edit physical characteristics dialog.ui', self)
@@ -235,6 +267,7 @@ class EditCharacteristicsDialog(QDialog):
     def click(self):
         self.clicked = True
 
+        # формулы для вычисления норм
         coeff = {'Низкий': 1.25, 'Умеренный': 1.5, 'Интенсивный': 1.75}[self.physical_activity_level.currentText()]
         if self.sex.currentText() == 'Мужской':
             self.calorie = (10 * self.weight.value() + 6.25 * self.growth.value() + 5 * self.age.value() + 5) * coeff
@@ -247,20 +280,20 @@ class EditCharacteristicsDialog(QDialog):
         self.close()
 
 
-class ExitDialog(QDialog):
+class ExitDialog(QDialog):  # диалог при выходе
     def __init__(self):
         super().__init__()
         uic.loadUi('./source/exit dialog.ui', self)
-        self.no_clicked = True
+        self.no_clicked = False
 
         self.yes_btn.clicked.connect(self.yes)
         self.no_btn.clicked.connect(self.no)
 
     def yes(self):
-        self.no_clicked = False
         self.close()
 
     def no(self):
+        self.no_clicked = True  # нажато кнопка "нет"
         self.close()
 
 
